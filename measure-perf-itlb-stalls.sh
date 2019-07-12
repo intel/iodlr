590fa0632e6ae614990bf0169935db95fbb7b6bc
Uttam Pawar <uttam.c.pawar@intel.com>
2019-07-12 12:07:34 -0700

    Added few checks, a function to calculate new metric as an example

--- measure-perf-itlb-stalls.sh
+++ measure-perf-itlb-stalls.sh
@@ -19,7 +19,16 @@ NCLUSTERS=1 #1, 2, 4, 8
 NPROCS=1    #1, 4, 8, 14
 
 PERF_DATA_COLLECTION_TIME=10 #perf collection time in seconds
+
+#Individual sets of pmus
+#ITLB_STALLS
 PERF_PMUS="cycles,icache_64b.iftag_stall"
+
+#L2_demand_code_MPI
+PERF_PMUS="instructions,l2_rqsts.code_rd_miss"
+
+#One big set of pmus
+PERF_PMUS="cycles,icache_64b.iftag_stall,instructions,l2_rqsts.code_rd_miss,l2_rqsts.all_code_rd"
 
 PROCESS_ID=0 #Initialization
 PERF_DATA_FILE="perf_stat.txt"
@@ -68,7 +77,7 @@ function start_client_for_perf_data() {
   echo 
 }
 
-function get_perf_data() {
+function collect_perf_data() {
   echo 
 
   start_client_for_perf_data
@@ -81,23 +90,112 @@ function get_perf_data() {
   echo 
 }
 
-function get_perf_metric() {
-  echo
-  echo "--------------------------------------------------"
-  echo "`date`:Calculate the final metric"
-  echo "--------------------------------------------------"
-  cat $PERF_DATA_FILE
-  #a=icache_64b.iftag_stall
-  #b=cycles
-  #metric_ITLB_Misses(%) = 100*(a/d)
+function check_if_pmu_exists() {
+  local OLDIFS=$IFS
+  IFS=","; read -ra PMUS <<< "${PERF_PMUS}"
 
-  cycles=`grep cycles $PERF_DATA_FILE |cut -d 'c' -f1 | tr -d '[:space:][,]'`
-  iftag_stall=`grep icache_64b.iftag_stall $PERF_DATA_FILE | cut -d 'i' -f1 | tr -d '[:space:][,]'`
-  echo "Cycles=${cycles}"
-  echo "Stalls=${iftag_stall}"
-  metric_ITLB_MISSES=`echo "100*(${iftag_stall}/${cycles})"| bc -l`
-  #metric_ITLB_MISSES=`bc -l < echo "scale=2; 100*(${iftag_stall}/${cycles})"`
-  echo "metric_ITLB_Misses(%)=${metric_ITLB_MISSES}"
+  local pmu_not_found=0
+  for i in "${PMUS[@]}"; do
+    pmu_value=`grep ${i} $PERF_DATA_FILE |cut -d 'c' -f1 | tr -d '[:space:][,]'`
+    if [[ "x${pmu_value}" == "x" ]]; then
+      echo "PMU ($i) not found with valid value"
+      pmu_not_found=1
+    fi
+  done
+  IFS=$OLDIFS
+
+  if [[ ${pmu_not_found} == 1 ]]; then
+    echo "Error: Missing required one or more PMUs ($PERF_PMUS)"
+    echo "Here is the perf output data"
+    echo
+    cat $PERF_DATA_FILE
+    echo
+    echo "Aborting ...."
+    exit 1
+  fi
+
+}
+
+function return_pmu_value() {
+  if [ "x${1}" == "x" ]; then
+    echo "WARNING: No pmu string specified"
+    echo 0
+  else
+    local myresult=`grep ${1} $PERF_DATA_FILE | awk '{print $1}' | tr -d '[:space:][,]'`
+    echo "$myresult"
+  fi
+}
+
+function get_itlb_stall_metric() {
+  echo
+  echo "================================================="
+  echo "Calculate the final itlb_stall metric"
+  echo "--------------------------------------------------"
+  echo "FORMULA: metric_ITLB_Misses(%) = 100*(a/b)"
+  echo "         where, a=icache_64b.iftag_stall"
+  echo "                b=cycles"
+  echo "================================================="
+
+  # High level PMU check function
+  check_if_pmu_exists
+
+  local a=`return_pmu_value "icache_64b.iftag_stall"`
+  local b=`return_pmu_value "cycles"`
+
+  local metric=`echo "scale=3;100*(${a}/${b})"| bc -l`
+  echo "metric_ITLB_Misses(%)=${metric}"
+  echo
+}
+
+function get_l2_demand_code_MPI_metric() {
+  echo
+  echo "================================================="
+  echo "Calculate the final l2_demand_code MPI metric"
+  echo "--------------------------------------------------"
+  #b=inst_retired.any
+  echo "FORMULA: metric_L2_demand_code_MPI = (a/b)"
+  echo "         where, a=l2_rqsts.code_rd_miss"
+  echo "                b=instructions"
+  echo "================================================="
+
+  # High level PMU check function
+  check_if_pmu_exists
+
+  local a=`return_pmu_value "l2_rqsts.code_rd_miss"`
+  local b=`return_pmu_value "instructions"`
+
+  local metric=`echo "scale=3;(${a}/${b})"| bc -l`
+  echo "metric_L2_demand_code_MPI=${metric}"
+  echo
+}
+
+function get_l1_code_read_MPI_metric() {
+  echo
+  echo "================================================="
+  echo "Calculate the final l1_code_read_miss_per_instr"
+  echo "--------------------------------------------------"
+  echo "FORMULA: metric_L1_code_read_MPI = (a/b)"
+  echo "         where, a=l2_rqsts.all_code_rd"
+  echo "                b=instructions"
+  echo "================================================="
+
+  # High level PMU check function
+  check_if_pmu_exists
+
+  local a=`return_pmu_value "l2_rqsts.all_code_rd"`
+  local b=`return_pmu_value "instructions"`
+
+  local metric=`echo "scale=3;(${a}/${b})"| bc -l`
+  echo "metric_L1_code_read_MPI=${metric}"
+  echo
+}
+
+function display_perf_data() 
+{
+  echo
+  echo "Here is the perf output data"
+  echo
+  cat $PERF_DATA_FILE
   echo
 }
 
@@ -105,7 +203,6 @@ function app_specific_phases() {
   warm_up_phase
   #get_rps_phase
 }
-
 
 function stop_application() {
   echo 
@@ -121,21 +218,24 @@ function cleanup() {
 # Reset the application state
 cleanup
 
+# Function to start an application
 #start_application
 
 # This is optional
 #app_specific_phases #This includes warmup phase for Ghost and measurement phass.
 
 # Get perf data
-#get_perf_data
+#collect_perf_data
 
-# Reset the application state
+# Reset the application state (stop or kill all processes)
 cleanup
 
-# Calculate the perf metric
-get_perf_metric
+# Calculate the perf metric, we can add more as needed
+display_perf_data
+
+get_itlb_stall_metric
+#get_l2_demand_code_MPI_metric
+get_l1_code_read_MPI_metric
 
 exit
-
-
 
