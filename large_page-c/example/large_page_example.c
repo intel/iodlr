50b80c168a7d3e4ce2fe2ff4b62b269690c9338d
Catalin Gabriel Manciu <catalin.gabriel.manciu@intel.com>
2019-12-03 14:02:58 -0800

    source: Move map_status strings to library
    Add MapStatusStr which returns a string representing the map_status
    value given as parameter. The second parameter controls whether it
    returns a short version (the name of the error as it appears in the
    map_status enum), or the actual description of the error.
    Add two macros to simplify usage: MAP_STATUS_STR(status) will return
    the full description, MAP_STATUS_STR_SHORT(status) will return
    the shorter version.
    Move map_status_text from large_page_example.c to large_page.c and
    expand it to include the short versions of the strings.

--- large_page-c/example/large_page_example.c
+++ large_page-c/example/large_page_example.c
@@ -1,33 +1,6 @@
 #include "large_page.h"
 #include <stdio.h>
 
-static char* map_status_text[] = {
-  "ok",
-  "failed to read executable path file",
-  "failed to open thp enablement status file",
-  "invalid regex",
-  "invalid region boundaries",
-  "malformed thp enablement status file",
-  "malformed /proc/<PID>/maps file",
-  "failed to open maps file",
-  "the remapping function is part of the region",
-  "regex was NULL",
-  "map region not found",
-  "map region too small",
-  "see errno",
-  "madvise for destination failed",
-  "madvise for destination and unmapping of temporary failed",
-  "madvise for destination and unmappings failed",
-  "madvise for destination and unmapping of destination failed",
-  "mapping of destination failed",
-  "mapping of destination and unmapping of temporary failed",
-  "mprotect failed",
-  "mprotect and unmapping of temporary failed",
-  "mprotect and unmappings failed",
-  "mprotect and unmapping of destination failed",
-  "unmapping of temporary failed",
-};
-
 int main() {
   map_status status;
   bool is_enabled;
@@ -35,7 +8,7 @@ int main() {
   status = IsLargePagesEnabled(&is_enabled);
   if (status != map_ok) {
     fprintf(stderr, "Failed to check enablement: %s\n",
-            map_status_text[status]);
+            MAP_STATUS_STR(status));
     return status;
   }
 
@@ -43,12 +16,12 @@ int main() {
     printf("Transparent Huge Pages are enabled, mapping...\n");
     status = MapStaticCodeToLargePages();
     if (status != map_ok) {
-      fprintf(stderr, "Failed to map: %s\n", map_status_text[status]);
+      fprintf(stderr, "Failed to map: %s\n", MAP_STATUS_STR(status));
       return status;
     }
     printf("Success\n");
-  } else {
-      printf("Transparent Huge Pages are not enabled\n");
+    return 0;
   }
-  return 0;
+  fprintf(stderr, "Transparent Huge Pages are not enabled\n");
+  return -1;
 }
