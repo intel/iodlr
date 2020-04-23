5b3db25a283a2d899d9a1e76dacb15a6ee4d1cd9
Gabriel Schulhof <gabriel.schulhof@intel.com>
2020-04-23 13:30:41 -0700

    lppreload: abort when mapping fails

--- large_page-c/lp_preload.c
+++ large_page-c/lp_preload.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "large_page.h"
 
 void __attribute__((constructor)) map_to_large_pages() {
-  bool is_enabled;
+  bool is_enabled = true;
   map_status status = IsLargePagesEnabled(&is_enabled);
-  if (status != map_ok) {
-    fprintf(stderr,
-            "Failed to determine if large pages is enabled: %s\n",
-            MapStatusStr(status, true));
-  }
+  if (status != map_ok) goto fail;
 
-  if (!is_enabled) {
-    fprintf(stderr, "Mapping to large pages is not enabled\n");
-    return;
-  }
+  if (!is_enabled) goto fail;
 
   status = MapStaticCodeToLargePages();
-  if (status != map_ok) {
+  if (status != map_ok) goto fail;
+  return;
+fail:
+  if (status == map_ok) {
+    if (!is_enabled)
+      fprintf(stderr,
+              "Mapping to large pages in not enabled on your system. "
+              "Make sure /sys/kernel/mm/transparent_hugepage/enabled is set to "
+              "'madvise' or 'enabled'\n");
+  } else {
     fprintf(stderr,
-            "Failed to map to large pages: %s\n",
+            "Mapping to large pages failed: %s\n",
             MapStatusStr(status, true));
-    return;
   }
-
-  fprintf(stderr, "Successfully mapped code to large pages\n");
+  abort();
 }
