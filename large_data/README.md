27092f40500421f6f6b551bf0491f45f68c8dae2
Suresh Srinivas <suresh.srinivas@intel.com>
2020-07-29 18:47:38 -0700

    Data large Reference Implementation (#35)
    * Data Large Pages Reference
    
    * Updates to PR
    
    * Updates2 to PR

---
+++ large_data/README.md
@@ -0,0 +1,63 @@
+# Reference Implementation for Utilizing Large Pages for Data
+This directory contains a simple reference implementation for automating the process
+of utilizing explicit huge pages for Data.
+
+
+# APIs
+```
+* bool iodlr_hp_enabled()
+  Check whether explict huge pages is enabled on the system
+  Returns true if Huge Pages is enabled on the system
+* size_t iodlr_get_hp_size()
+  Obtains the size of the explicit huge pages (2MB or 1GB) in bytes
+* void * iodlr_allocate(size_t s, size_t pgsz)
+  mmap size bytes with pgsz
+* void iodlr_deallocate(char *d, size_t s)
+```
+
+# Test
+``` 
+* int64_t hptest()
+  Allocate a SIZE buffer with Huge Pages, zero fill it, and stride through it at 4K stride touching each byte of the 4K region.
+  Returns the cycles (measured by rdtsc) to execute the test.
+
+* int64_t defaulttest()
+  Allocate a SIZE buffer with 4K Pages, zero fill it, and stride through it at 4K stride touching each byte of the 4K region.
+  Returns the cycles (measured by rdtsc) to execute the test.
+```
+
+# Infrastructure
+
+The Linux kernel in modern distros has the large pages feature enabled by default. To check whether this is true for your kernel, use the following command and look for output lines containing “huge”:
+```
+shell> cat /proc/meminfo | grep -i huge
+HugePages_Total:       0
+HugePages_Free:        0
+HugePages_Rsvd:        0
+HugePages_Surp:        0
+Hugepagesize:       1048676 kB
+```
+The nonempty command output indicates that large page support is present, but the zero values indicate that no pages are configured for use. Also the kernel is configured to use 1GB pages
+
+Set the number of pages to be used.
+Each page is 1GB, so a value of 8 = 8GB.
+This command actually allocates memory, so the requested amount of memory must be available.
+```
+echo 8 > /proc/sys/vm/nr_hugepages
+``` 
+
+# Building
+```
+  make
+```
+
+# Testing
+This test needs 8G memory and for the hptest it needs 8 * 1G pages or 4096 * 2MB pages allocated as hugepages (using `nr_hugepages`)
+```
+./data-large-reference 
+hptest hpsize 2097152
+Cycles for 2097152 = 4918657468
+defaulttest default pagesize 4096
+Cycles for 4096 = 10988167648
+Huge Page Data Speedup = 2.23398
+```
---
+++ large_data/data-large-reference.cc
@@ -0,0 +1,199 @@
+// Copyright (C) 2018 Intel Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+//
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <x86intrin.h>
+
+#ifndef MAP_HUGETLB
+# define MAP_HUGETLB 0x40000
+#endif
+
+#ifndef MAP_HUGE_1GB
+# define MAP_HUGE_1GB (30 << 26)
+#endif
+
+# define FLAGS_1G MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB | MAP_HUGE_1GB
+# define FLAGS_2M MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB
+# define FLAGS_4K MAP_ANONYMOUS | MAP_PRIVATE
+
+
+/* 8G memory allocation */
+#define SIZE_8G 8ul << 30 
+/* Divide it into 4k chunks */
+#define NCHUNKS_8G (8589934592/4096)
+
+/* 4G memory allocation */
+#define SIZE_4G 4ul << 30 
+/* Divide it into 4k chunks */
+#define NCHUNKS_4G (4294967296/4096)
+
+/* 2G memory allocation */
+#define SIZE_2G 2ul << 30 
+/* Divide it into 4k chunks */
+#define NCHUNKS_2G (2147483648/4096)
+
+#define SIZE SIZE_8G
+#define NCHUNKS NCHUNKS_8G
+
+
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+#include <unistd.h>
+size_t iodlr_get_default_page_size() {
+return (size_t)(sysconf(_SC_PAGESIZE));
+}
+
+using std::string;
+using std::ifstream;
+using std::istringstream;
+using std::cout;
+using std::getline;
+
+uint64_t iodlr_procmeminfo(string key) {
+    ifstream ifs("/proc/meminfo");
+    string map_line;
+
+    while(getline(ifs,map_line)){
+        istringstream iss(map_line);
+        string keyname;
+        uint64_t value;
+        string kb;
+        iss >> keyname;
+        iss >> value;
+        iss >> kb;
+        if (keyname == key) {
+                return value;
+        }
+    }
+    return -1; // nothing found
+
+}
+
+bool iodlr_hp_enabled() {
+    uint64_t val = iodlr_procmeminfo("HugePages_Total:");
+    if (val > 0 )
+        return true;
+    else 
+        return false;
+}
+
+size_t iodlr_get_hp_size() {
+    uint64_t val = iodlr_procmeminfo("Hugepagesize:");
+    if (val > 0) {
+        return val * 1024;
+    }
+    return -1;
+}
+
+
+void * iodlr_allocate(size_t s, size_t pgsz) {
+    int flags=FLAGS_4K;
+    void *data;
+    if (pgsz ==  1048576* 1024)
+       flags = FLAGS_1G;
+    else if (pgsz == 2048*1024)
+       flags = FLAGS_2M;
+
+    data = mmap(NULL, s,
+                         PROT_READ | PROT_WRITE, flags,
+                         -1, 0);
+    assert(data != MAP_FAILED);
+    return data;
+}
+
+void iodlr_deallocate(char *d, size_t s) {
+    int i;
+    i = munmap (d, s);
+    assert (i != -1);
+}
+
+void zerofill(char *d, size_t s) {
+    memset (d, 0, s);
+}
+
+void touch(char *d, size_t stride, size_t index, size_t size) {
+
+    char a;
+    int i;
+    assert (stride < size);
+
+    for (i=0; i < NCHUNKS; i++) {
+        assert (i*stride + index < size);
+        a = d[i*stride+index];
+    }
+}
+
+int64_t dotest(size_t s) {
+        int i;
+        size_t stride = (SIZE)/NCHUNKS;
+        uint64_t start, end;
+        start = __rdtsc();
+        char *data = (char *)iodlr_allocate(SIZE, s);
+        zerofill(data, SIZE);
+        for (i=0; i < 4096; i++) {
+          touch(data, stride, i, SIZE);
+        }
+        iodlr_deallocate(data, SIZE);
+        end = __rdtsc();
+        cout << "Cycles for " << s << " = " << (end - start) << "\n";
+        return (end - start);
+}
+
+int64_t hptest() {
+        bool lp = iodlr_hp_enabled();
+        if (lp == true) {
+          size_t l = iodlr_get_hp_size();
+          cout << "hptest hpsize " << l << "\n";
+          return dotest(l);
+        }
+        return -1;
+}
+
+int64_t defaulttest() {
+        size_t d = iodlr_get_default_page_size();
+        cout << "defaulttest default pagesize " << d << "\n";
+        return dotest(d);
+}
+
+
+int main (int argc, char **argv)
+{
+        (void)argc;
+        (void)argv;
+        int64_t hpt, dt;
+        hpt = hptest();
+        dt = defaulttest();
+        cout << "Huge Page Data Speedup = " << (double) dt/ (double)hpt << "\n";
+}
+
+
+
